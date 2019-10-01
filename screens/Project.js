7af1745dfbfe1e37d3990823e566acb298b6e470
karolinakazmierska <karo.a.kazmierska@gmail.com>
2019-10-01 13:19:20 +0200

    Added animated view on Project screen

--- screens/Project.js
+++ screens/Project.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { StyleSheet, Text, View, TouchableOpacity, Button } from "react-native";
+import React, { Component, useState, useEffect } from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Button, Animated } from "react-native";
 import { NavigationActions } from "react-navigation";
 import firebase from './../utils/firebase';
 import { myStyles } from './../utils/styles';
@@ -12,9 +12,60 @@ export default class Project extends Component {
 
 	render() {
 		const { navigation } = this.props;
+
+		const FadeInLeftTop = (props) => {
+			const [fadeAnim] = useState(new Animated.Value(0))
+			React.useEffect(() => {
+				Animated.timing(fadeAnim, { toValue: 1, delay: 1000, duration: 800 }).start();
+			}, [])
+			return (
+				<Animated.View style={{...props.style, opacity: fadeAnim}}>{props.children}</Animated.View>
+			);
+		}
+
+		const FadeInRightTop = (props) => {
+			const [fadeAnim] = useState(new Animated.Value(0))
+			React.useEffect(() => {
+				Animated.timing(fadeAnim, { toValue: 1, delay: 1500, duration: 800 }).start();
+			}, [])
+			return (
+				<Animated.View style={{...props.style, opacity: fadeAnim}}>{props.children}</Animated.View>
+			);
+		}
+
+		const FadeInLeftBottom = (props) => {
+			const [fadeAnim] = useState(new Animated.Value(0))
+			React.useEffect(() => {
+				Animated.timing(fadeAnim, { toValue: 1, delay: 2000, duration: 800 }).start();
+			}, [])
+			return (
+				<Animated.View style={{...props.style, opacity: fadeAnim}}>{props.children}</Animated.View>
+			);
+		}
+
+		const FadeInRightBottom = (props) => {
+			const [fadeAnim] = useState(new Animated.Value(0))
+			React.useEffect(() => {
+				Animated.timing(fadeAnim, { toValue: 1, delay: 2500, duration: 800 }).start();
+			}, [])
+			return (
+				<Animated.View style={{...props.style, opacity: fadeAnim}}>{props.children}</Animated.View>
+			);
+		}
+
 		return (
 			<View style={styles.container}>
 				<Text style={styles.welcome}>Add guests to your project and assign them to tables of your choice</Text>
+
+				<View style={{marginBottom: 160}}>
+					<View style={styles.table}>
+						<FadeInLeftTop style={styles.chair1}><Text>Martha</Text></FadeInLeftTop>
+						<FadeInRightTop style={styles.chair2}><Text>John</Text></FadeInRightTop>
+						<FadeInLeftBottom style={styles.chair3}><Text>Susan</Text></FadeInLeftBottom>
+						<FadeInRightBottom style={styles.chair4}><Text>Bob</Text></FadeInRightBottom>
+					</View>
+				</View>
+
 				<View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
 					<TouchableOpacity
 	          			style={styles.btn}
@@ -94,5 +145,81 @@ const styles = StyleSheet.create({
 		textTransform: 'uppercase',
 		alignSelf: "center",
 		textAlign: "center",
-	}
+	},
+	table: {
+		borderStyle: "solid",
+		borderColor: myStyles.colors.dark,
+		backgroundColor: myStyles.colors.dark,
+		borderRadius: 140,
+		height: 140,
+		width: 140,
+		overflow: "visible",
+	},
+	chair: {
+		backgroundColor: myStyles.colors.light,
+		borderRadius: 60,
+		height: 60,
+		width: 60,
+		position: "absolute",
+		textAlign: "center",
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	chair1: {
+		backgroundColor: myStyles.colors.light,
+		borderRadius: 60,
+		height: 60,
+		width: 60,
+		position: "absolute",
+		textAlign: "center",
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+		left: -10,
+		top: -5,
+		overflow: "hidden"
+	},
+	chair2: {
+		backgroundColor: myStyles.colors.light,
+		borderRadius: 60,
+		height: 60,
+		width: 60,
+		position: "absolute",
+		textAlign: "center",
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+		right: -10,
+		top: -5,
+		overflow: "hidden"
+	},
+	chair3: {
+		backgroundColor: myStyles.colors.light,
+		borderRadius: 60,
+		height: 60,
+		width: 60,
+		position: "absolute",
+		textAlign: "center",
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+		left: -10,
+		top: 90,
+		overflow: "hidden"
+	},
+	chair4: {
+		backgroundColor: myStyles.colors.light,
+		borderRadius: 60,
+		height: 60,
+		width: 60,
+		position: "absolute",
+		textAlign: "center",
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+		right: -10,
+		top: 90,
+		overflow: "hidden"
+	},
 });
--- utils/styles.js
+++ utils/styles.js
@@ -1,6 +1,7 @@
 export const myStyles = {
     colors: {
-        light: '#EFDBDC',
+        // light: '#EFDBDC',
+        light: '#e8e8e8',
         dark: '#f8049c',
         lightgrey: '#e6e6e6',
         darkgrey: '',
